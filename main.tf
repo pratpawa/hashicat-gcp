929e6d4b9f23b96c75acf5849b73d63c9af0c6f8
pratpawa <pratpawa16@gmail.com>
2021-05-28 11:33:05 +0000

    Added the first label

--- main.tf
+++ main.tf
@@ -48,6 +48,7 @@ resource "google_compute_instance" "hashicat" {
   zone         = "${var.region}-b"
   machine_type = var.machine_type
 
+
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-1804-lts"
@@ -68,6 +69,7 @@ resource "google_compute_instance" "hashicat" {
 
   labels = {
     name = "hashicat"
+    department="devops"
   }
 
 }
