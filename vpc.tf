e5ba00b2d4b25296020ff06e7827e1595913e3b5
pratpawa <pratpawa16@gmail.com>
2021-05-28 12:05:00 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "network" {
+  source  = "terraform-google-modules/network/google"
+  version = "3.2.2"
+  # insert the 3 required variables here
+  network_name="gaurav-network"
+  project_id="var.project"
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+  
+}
